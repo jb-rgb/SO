ef29e5af17a7b4d84e0ea2d02e629529008c8a0e
Jorge Barahona <bacj010509@gs.utm.mx>
2023-03-29 14:44:13 -0600

    upgrade

--- Programas/t3.c
+++ Programas/t3.c
@@ -8,19 +8,19 @@
 #include <sys/wait.h>
 #include <ctype.h>
 #include <stdlib.h>
+#define MAX_LINE 1024 // La máxima longitud del comando
 
-#define MAX_LINE 1024 // The maximum length command
-
-//bandera para esperar los procesos que aun no terminan
+// Bandera para esperar los procesos que aun no terminan
 int espera = 1;
-char * tokenize(char *entrada);
+
+char *tokenizar(char *entrada);
 void redireccion_salida(char *Archivo_nombre);
 void redireccion_entrada(char *Archivo_nombre);
 void crear_Tuberia(char *args[]);
 
 int main(void)
 {
-    char *args[MAX_LINE]; // command line arguments
+    char *args[MAX_LINE]; // Argumentos de línea de comando
     
     while (1) {
         printf("$ ");
@@ -30,7 +30,7 @@ int main(void)
         char prompt[MAX_LINE];
         fgets(prompt, MAX_LINE, stdin);
         char *tokens;
-        tokens = tokenize(prompt);
+        tokens = tokenizar(prompt);
          // Salir del bucle si el usuario ingresa "exit"
         if (strcmp(tokens, "exit") == 0) {
             break;
@@ -43,23 +43,23 @@ int main(void)
         while (arg != NULL) {
             if (strcmp(arg, "<") == 0) {
                 // printf("ENtrada \n");
-                redireccion_entrada(strtok(NULL, " "));//con strtok se le asigna el nombre del archivo, por que se toma el siguiente parametro
+                redireccion_entrada(strtok(NULL, " "));// Con strtok se le asigna el nombre del archivo, por que se toma el siguiente parametro
             } else if (strcmp(arg, ">") == 0) {
                 // printf("salida \n");
-                redireccion_salida(strtok(NULL, " "));//con strtok se le asigna el nombre del archivo, por que se toma el siguiente parametro
+                redireccion_salida(strtok(NULL, " "));// Con strtok se le asigna el nombre del archivo, por que se toma el siguiente parametro
             } else if (strcmp(arg, "|") == 0) {
                 // printf("Tuberia \n");
                 args[i] = NULL;
                 crear_Tuberia(args);
-                i = 0;//iniciar desde 0 cuando se encuentra una tuberia , para que despues se ejecuten los comandos correspondientes 
+                i = 0;// Iniciar desde 0 cuando se encuentra una tuberia , para que despues se ejecuten los comandos correspondientes 
             } else {
-                args[i] = arg;//el comando se va a agregar al arreglo de comandos 
-                i++;//se incrementa la posicion 
+                args[i] = arg;// El comando se va a agregar al arreglo de comandos 
+                i++;// Se incrementa la posicion 
             }
             arg = strtok(NULL, " ");
         }
         args[i] = NULL;
-  //ejecutar el proceso si no hay | , < , > 
+  // Ejecutar el proceso si no hay | , < , > 
           pid_t pid;
     
             pid = fork();
@@ -67,12 +67,12 @@ int main(void)
                 perror("fork");
                 exit(EXIT_FAILURE);
             }
-            else if ( pid == 0) { //Se crea el proceso hijo
+            else if ( pid == 0) { // Se crea el proceso hijo
                 execvp(args[0], args);
                 perror(args[0]);
                 exit(EXIT_FAILURE);
             }
-            else {//Si no es el proceso padre
+            else {//S i no es el proceso padre
                 if (espera) {
                     waitpid(pid, NULL, 0);
                 } else {
@@ -85,86 +85,71 @@ int main(void)
     return 0;
 }
 
-
-void redireccion_entrada(char *Archivo_nombre)
-{
+void redireccion_entrada(char *Archivo_nombre) {
     int entrada = open(Archivo_nombre, O_RDONLY);
     dup2(entrada, 0);
     close(entrada);
 }
 
-
-void redireccion_salida(char *Archivo_nombre)
-{
+void redireccion_salida(char *Archivo_nombre) {
     int salida = open(Archivo_nombre, O_WRONLY | O_TRUNC | O_CREAT, 0600);
     dup2(salida, 1);
     close(salida);
 }
 
-void crear_Tuberia(char *args[])
-{
+void crear_Tuberia(char *args[]) {
     int fd[2];
     pipe(fd);
 
     dup2(fd[1], 1);
     close(fd[1]);
-
-    //printf("args = %s\n", *args);
-
-      pid_t pid;
+    pid_t pid;
     
-            pid = fork();
-             if (pid == -1) { 
-                perror("fork");
-                exit(EXIT_FAILURE);
-            }else if ( pid == 0) { /* child process */
-                execvp(args[0], args);
-                perror(args[0]);
-                exit(EXIT_FAILURE);
-            }
-            else { /* parent process */
-                if (espera) {
-                    waitpid(pid, NULL, 0);
-                } else {
-                    espera = 0;
-                }
-            }
-            redireccion_entrada("/dev/tty");
-            redireccion_salida("/dev/tty");
+    pid = fork();
+        if (pid == -1) { 
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }else if ( pid == 0) { /* Proceso hijo */
+        execvp(args[0], args);
+        perror(args[0]);
+        exit(EXIT_FAILURE);
+    }
+    else { /* Proceso padre */
+        if (espera) {
+            waitpid(pid, NULL, 0);
+        } else {
+            espera = 0;
+        }
+    }
+    redireccion_entrada("/dev/tty");
+    redireccion_salida("/dev/tty");
 
     dup2(fd[0], 0);
     close(fd[0]);
 }
 
-/**
- * Creates a tokenized form of the input with spaces to separate words.
- * 
- * @param  *input the input string
- * @return tokenized the tokenized stirng
- */
-char * tokenize(char *entrada)
-{
+char *tokenizar(char *entrada) {
     int i;
     int j = 0;
-    char *tokenized = (char *)malloc((MAX_LINE * 2) * sizeof(char));
+    char *tokenizado = (char *)malloc((MAX_LINE * 2) * sizeof(char));
 
-    // add spaces around special characters
+    // Agregar espacios alrededor de los caracteres especiales
     for (i = 0; i < strlen(entrada); i++) {
         if (entrada[i] != '>' && entrada[i] != '<' && entrada[i] != '|') {
-            tokenized[j++] = entrada[i];
+            tokenizado[j++] = entrada[i];
         } else {
-            tokenized[j++] = ' ';
-            tokenized[j++] = entrada[i];
-            tokenized[j++] = ' ';
+            tokenizado[j++] = ' ';
+            tokenizado[j++] = entrada[i];
+            tokenizado[j++] = ' ';
         }
     }
-    tokenized[j++] = '\0';
+    tokenizado[j++] = '\0';
 
-    //poner el caracter null al final del arreglo 
+    // Poner el caracter null al final del arreglo 
     char *end;
-    end = tokenized + strlen(tokenized) - 1;
+    end = tokenizado + strlen(tokenizado) - 1;
     end--;
     *(end + 1) = '\0';
     
-    return tokenized;
+    return tokenizado;
 }
